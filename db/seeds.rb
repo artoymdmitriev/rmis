6a0ca637ce34ecdab109b56f8cadfd6e0c724ab4
artyom <admitriev5813@gmail.com>
2017-11-18 23:22:51 +0300

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,52 +7,43 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('12-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
+            occured_at: Date.parse('20-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
 
 Risk.create(name: 'Тупой персонал', description: 'Персонал организации не выполняет обязанности', category: :c2,
-            occured_at: Date.parse('13-07-2017'), danger_rate: 2, origin_type: :internal, user_id: 1)
-
-Risk.create(name: 'Отсутствие СЭД', description: 'СЭД\'а нет!!', category: :c2,
-            occured_at: Date.parse('13-07-2017'), danger_rate: 2, origin_type: :external, user_id: 1)
+            occured_at: Date.parse('21-07-2017'), danger_rate: 2, origin_type: :internal, user_id: 1)
 
 Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('12-07-2017'), danger_rate: 4, origin_type: :internal, user_id: 1)
+            occured_at: Date.parse('23-07-2017'), danger_rate: 4, origin_type: :internal, user_id: 1)
 
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('12-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
+Risk.create(name: 'Тупой персонал', description: 'Персонал организации не выполняет обязанности', category: :c2,
+            occured_at: Date.parse('22-07-2017'), danger_rate: 5, origin_type: :internal, user_id: 1)
 
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('14-07-2017'), danger_rate: 5, origin_type: :external, user_id: 1)
+Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c3,
+            occured_at: Date.parse('25-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
 
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('15-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
+Risk.create(name: 'Тупой персонал', description: 'Персонал организации не выполняет обязанности', category: :c4,
+            occured_at: Date.parse('26-07-2017'), danger_rate: 1, origin_type: :internal, user_id: 1)
 
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('15-07-2017'), danger_rate: 1, origin_type: :external, user_id: 1)
+Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c4,
+            occured_at: Date.parse('30-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
 
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('16-07-2017'), danger_rate: 2, origin_type: :external, user_id: 1)
-
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('16-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
-
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('16-07-2017'), danger_rate: 3, origin_type: :external, user_id: 1)
-
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('16-07-2017'), danger_rate: 4, origin_type: :internal, user_id: 1)
-
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('16-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
-
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('19-07-2017'), danger_rate: 5, origin_type: :external, user_id: 1)
-
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
+Risk.create(name: 'Тупой персонал', description: 'Персонал организации не выполняет обязанности', category: :c3,
             occured_at: Date.parse('18-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
 
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('18-07-2017'), danger_rate: 3, origin_type: :external, user_id: 1)
+Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c2,
+            occured_at: Date.parse('25-07-2017'), danger_rate: 5, origin_type: :internal, user_id: 1)
 
-Risk.create(name: 'Плохой персонал', description: 'Персонал организации не выполняет обязанности', category: :c1,
-            occured_at: Date.parse('19-07-2017'), danger_rate: 2, origin_type: :internal, user_id: 1)
+Risk.create(name: 'Тупой персонал', description: 'Персонал организации не выполняет обязанности', category: :c5,
+            occured_at: Date.parse('23-07-2017'), danger_rate: 2, origin_type: :internal, user_id: 1)
+
+Risk.create(name: 'Тупой персонал', description: 'Персонал организации не выполняет обязанности', category: :c6,
+            occured_at: Date.parse('27-07-2017'), danger_rate: 4, origin_type: :internal, user_id: 1)
+
+Risk.create(name: 'Тупой персонал', description: 'Персонал организации не выполняет обязанности', category: :c3,
+            occured_at: Date.parse('26-07-2017'), danger_rate: 2, origin_type: :internal, user_id: 1)
+
+Risk.create(name: 'Тупой персонал', description: 'Персонал организации не выполняет обязанности', category: :c5,
+            occured_at: Date.parse('22-07-2017'), danger_rate: 3, origin_type: :internal, user_id: 1)
+
+Risk.create(name: 'Тупой персонал', description: 'Персонал организации не выполняет обязанности', category: :c5,
+            occured_at: Date.parse('28-07-2017'), danger_rate: 2, origin_type: :internal, user_id: 1)
